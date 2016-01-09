0034c568f0bb122d2e10a4c1c6aafd50f9b175d3
Ryan Hestin <hestinr12@gmail.com>
2016-01-08 21:28:09 -0500

    implements session based unique question viewing, admin login, and admin logout...plus the associated views

--- app.js
+++ app.js
@@ -5,7 +5,42 @@ var bodyParser = require('body-parser');
 var jade = require('jade');
 
 var app = express();
-var adminAccout = {'name': 'sumome', 'pass': 'tacos'};
+var adminAccout = {user: 'admin', pass: 'taco'};
+var tempQuestions = [
+	{
+		text: 'What kind of tacos do you like?',
+		questionId: 1,
+		choices: [
+			[1, 'crispy chickennnnnnnnnn nnnnnnnnnnnn nnnnnnnnnnnnn nnnn nnnnnnnnnnnnnn nnnnnn nn'],
+			[2, 'soft chicken'],
+			[3, 'crispy shredded beef'],
+			[4, 'soft shredded beef'],
+			[5, 'soft fish'],
+			[6, 'soft veggie'],
+			[7, 'none :(']
+		]
+	},
+	{
+		text: 'What color is the sky?',
+		questionId: 2,
+		choices: [
+			[8, 'red'],
+			[9, 'blue'],
+			[10, 'green'],
+			[11, 'purple'],
+		]
+	},
+	{
+		text: 'Who\'s yo daddy?',
+		questionId: 3,
+		choices: [
+			[12, 'A Machine'],
+			[13, 'Spiderman'],
+			[14, 'Bob Ross'],
+			[15, 'You'],
+		]
+	}
+];
 
 /*
 	Template Engine
@@ -29,50 +64,36 @@ app.use(session({
 		POST '/:survey_id' - Receive the result of a url-encoded form
 */
 app.get('/', function (req, res){
-	//get a relevant question
-	var temp_question = [
-		{
-			text: 'What kind of tacos do you like?',
-			question_id: 1,
-			choices: [
-				[1, 'crispy chickennnnnnnnnn nnnnnnnnnnnn nnnnnnnnnnnnn nnnn nnnnnnnnnnnnnn nnnnnn nn'],
-				[2, 'soft chicken'],
-				[3, 'crispy shredded beef'],
-				[4, 'soft shredded beef'],
-				[5, 'soft fish'],
-				[6, 'soft veggie'],
-				[7, 'none :(']
-			]
-		},
-		{
-			text: 'What color is the sky?',
-			question_id: 2,
-			choices: [
-				[8, 'red'],
-				[9, 'blue'],
-				[10, 'green'],
-				[11, 'purple'],
-			]
-		},
-		{
-			text: 'Who\'s yo daddy?',
-			question_id: 3,
-			choices: [
-				[12, 'A Machine'],
-				[13, 'Spiderman'],
-				[14, 'Bob Ross'],
-				[15, 'You'],
-			]
-		}
-	];
+
+	//init session variables
+	if(!req.session.seenQuestions){
+		req.session.seenQuestions = [];
+	}
+	console.log(req.session.seenQuestions);
+
+	//get possibilities (!!!!!!! Done via DB query eventually !!!!!!!)
+	var possibilities = tempQuestions.filter(function (value) {
+		return req.session.seenQuestions.indexOf(value.questionId) == -1;
+	});
+
 	//package data for jade (once using DB))
+		//(when on the DB)
+
+	//pick question
+	var pick = Math.floor((Math.random() * possibilities.length));
+	
+	//mark session
+	try{
+		req.session.seenQuestions.push(possibilities[pick].questionId)
+	} catch(e) {
+		console.log(e);
+	}
 
 	//render
-	res.render('random_survey.jade', temp_question[Math.floor((Math.random() * 3))]);
+	res.render('random_survey.jade', possibilities[pick]);
 });
 
 app.post('/result', function (req, res){
-	//res.send(req.body.answer_id + ' picked for survey ' + req.params.survey_id);
 	console.log(req.body);
 	res.redirect('/')
 });
@@ -90,15 +111,32 @@ app.post('/result', function (req, res){
 		POST 'admin/new_survey' - Submit a new survey to be added in
 */
 app.get('/admin', function (req, res){
-	res.send('ADMIN SIGNIN');
+	if(req.session.admin) {
+		res.redirect('/admin/survey_questions');
+	} else {
+		res.render('admin_login.jade', {error: req.session.loginErr});
+	}
+
+	if(req.session.loginErr) {
+		delete req.session.loginErr;
+	}
 });
 
 app.post('/login', function (req, res){
-	res.send('user: ' + req.body.user + '| pass: ' + req.body.pass);
+	if (req.body.user == adminAccout.user && req.body.pass == adminAccout.pass){
+		req.session.admin = true;
+		req.session.loginErr = false;
+		res.redirect('/admin/survey_questions');
+	} else {
+		req.session.loginErr = true
+		res.redirect('/admin');
+	}
 });
 
 app.get('/logout', function(req, res){
-	res.send('logging out');
+	delete req.session.loginErr;
+	delete req.session.admin;
+	res.redirect('admin');
 });
 
 /*
